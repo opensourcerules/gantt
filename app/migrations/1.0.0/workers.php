872bc7980bd907be426f7e44ad6b651b6235cc93
Marius Pantea <marius.pantea@p4b.ro>
2018-05-07 08:46:09 +0300

    Database migration

---
+++ app/migrations/1.0.0/workers.php
@@ -0,0 +1,102 @@
+<?php
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class WorkersMigration_100
+ */
+class WorkersMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     * @throws \Phalcon\Db\Exception
+     */
+    public function up()
+    {
+        $this->morphTable(
+            'workers',
+            [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'last_name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'first_name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'last_name'
+                        ]
+                    ),
+                    new Column(
+                        'email',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 60,
+                            'after' => 'first_name'
+                        ]
+                    ),
+                    new Column(
+                        'password',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 255,
+                            'after' => 'email'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('email', ['email'], 'UNIQUE')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '1',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
---
+++ app/migrations/1.0.0/workers_projects.php
@@ -0,0 +1,75 @@
+<?php
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class WorkersProjectsMigration_100
+ */
+class WorkersProjectsMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     * @throws \Phalcon\Db\Exception
+     */
+    public function up()
+    {
+        $this->morphTable(
+            'workers_projects',
+            [
+                'columns' => [
+                    new Column(
+                        'worker_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'project_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'worker_id'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['worker_id', 'project_id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
