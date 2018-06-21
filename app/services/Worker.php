e12c0bfedd760fb9bed867f0f37ebf214d501259
Marius Pantea <marius.pantea@p4b.ro>
2018-06-21 08:33:12 +0300

    php7 return type for getWorker must be Model not Workers.

--- app/services/Worker.php
+++ app/services/Worker.php
@@ -87,7 +87,7 @@ class Worker
      * @param int $id
      * @return Workers
      */
-    public function getWorker(int $id): Workers
+    public function getWorker(int $id): Model
     {
         return Workers::findFirst([
             'id = :id:',
